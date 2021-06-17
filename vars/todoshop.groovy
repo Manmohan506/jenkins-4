002e34790cfef9940ea454b97bd2891f97693b21
Manmohan506 <manmohan@zelarsoft.com>
2021-06-17 16:15:11 +0530

    f

---
+++ vars/todoshop.groovy
@@ -0,0 +1,49 @@
+def call(Map params = [:] ) {
+     def args = [
+        NEXUS_IP : '172.31.15.73', 
+     ]
+     args << params
+    pipeline {
+        agent {
+            label "${args.SLAVE_LABEL}"
+        }
+    
+        environment {
+            COMPONENT ="${args.COMPONENT}"
+            NEXUS_IP = "${args.NEXUS_IP}"
+            PROJECT_NAME = "${args.PROJECT_NAME}"
+            SLAVE_LABEL = "${args.SLAVE_LABEL}"
+            APP_TYPE    = "${args.APP_TYPE}"
+        }
+        stages {
+            stage('Build code & install dependencies') {
+                steps {
+                    script {
+                        build = new nexus()
+                        build.code_build("${APP_TYPE}","${COMPONENT}")
+                    }
+                }
+
+            }
+            stage('Prepare Artifacts') {
+
+                steps {
+                    script {
+                        prepare = new nexus()
+                        prepare.make_artifacts("${APP_TYPE}","${COMPONENT}")
+                    }
+                }
+            }
+
+            stage('Upload Artifacts') {
+                steps {
+                    script {
+                        prepare = new nexus()
+                        prepare.nexus(COMPONENT)
+                    }
+
+                }
+            }
+        }
+    }
+}

8a57b3e04007614ca8978dfb0233f34a139e394a
Manmohan506 <manmohan@zelarsoft.com>
2021-08-14 00:15:32 +0530

    done

---
+++ docker.groovy
@@ -0,0 +1,35 @@
+def call(Map params = [:] ) {
+    def args = [
+           SLAVE_LABEL : "DOCKER"
+    ]
+    args << params
+
+    pipeline {
+      agent {
+        node {
+        label "${args.SLAVE_LABEL}"
+        }
+      }
+    
+        triggers {
+          pollSCM('* * * * 1-5') 
+        }
+        environment {
+            COMPONENT ="${args.COMPONENT}"
+            PROJECT_NAME = "${args.PROJECT_NAME}"
+            SLAVE_LABEL = "${args.SLAVE_LABEL}"
+            APP_TYPE    = "${args.APP_TYPE}"
+        }
+        stages {
+            stage('Build code & install dependencies') {
+                steps {
+                  sh '''
+                    docker build -t local .
+                  '''
+                }
+
+            }
+
+        }
+    }
+}
